33cb346e4ba8c225f479f26fd6617ce376c9b1b0
wendwin <wendwin12@gmail.com>
2024-07-12 11:07:18 +0700

    menggunakan binding text dengan directive & mustache

--- src/App.vue
+++ src/App.vue
@@ -1,45 +1,23 @@
 <script setup>
 // import HelloWorld from './components/HelloWorld.vue'
 // import TheWelcome from './components/TheWelcome.vue'
+const name = 'Batman';
+const greet = 'Hello';
+const channel = 'Codevolution';
+
 
 </script>
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
     <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+      <div class="">{{ name }} - {{ greet }}</div>
+      <div v-text="channel"></div>
     </div>
   </header>
 
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-}
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
 </style>
